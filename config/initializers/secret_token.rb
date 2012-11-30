8d49e49a2ab7c00575caaa2d21800cca25289f62
Dominic Ramms <dominic.ramms@fh-muenster.de>
2012-11-30 17:06:18 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageHoster::Application.config.secret_token = '9581d3a2036f9c0f3c5b5976a119be6597584498dadc982ebf341d3f69f845213a830143b5461f3a23aa257e3d0955943683226d80b6eb2f7184c651cae6b12c'
