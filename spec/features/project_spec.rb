4a49071bcb08a7e73be4e85adc7c26028af86d11
Dominic Ramms <dominic.ramms@fh-muenster.de>
2013-02-23 09:42:30 +0100

    routen geloescht1

---
+++ spec/features/project_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper.rb'
+
+describe Friend do
+
+	before :each do
+		User.new(:email => "dr@mymail.com", :first_name => "Dominic", :last_name => "Ramms", :password => "123456").save
+		User.new(:email => "sn@mymail.com", :first_name => "Sascha", :last_name => "Nonte", :password => "123456").save
+		User.new({"email"=>"mc@mymail.com", "first_name"=>"Marvin", "last_name"=>"Caspar", "password"=>"123456"}).save
+		User.new({"email"=>"tp@mymail.com", "first_name"=>"Tobias", "last_name"=>"Prein", "password"=>"123456"}).save
+
+		Friend.new(:user_id => 1, :friend_id => 2).save
+		Friend.new(:user_id => 2, :friend_id => 1).save
+		Friend.new(:user_id => 1, :friend_id => 3).save
+		Friend.new(:user_id => 3, :friend_id => 1).save
+
+		visit "/"
+		fill_in "user_email", :with => "dr@mymail.com"
+		fill_in "user_password", :with => "123456"
+		click_button "sign_in_button"
+	end
+
+	
+
+end
